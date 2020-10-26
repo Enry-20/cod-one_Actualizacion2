f024153f1098436eb08dd449dd39929dfec5a80d
Enry-20 <enriquecuellar7u7@gmail.com>
2020-10-26 05:54:46 +0100

    nuevas funciones y cambios generales

--- animation.js
+++ animation.js
@@ -3,15 +3,64 @@ function preesme(){
   window.location.href="https://getbootstrap.com/docs/4.5/components/collapse/"
 }
 enlace.onclick= preesme;
+// Scroll fuctions 
+let nav = document.getElementById("navbar")
 function scrolled(){
   let fondo= window.pageYOffset
-  if(fondo>770){document.getElementById("navbar").style.background="black"}
-  else{document.getElementById("navbar").style.background="none"}
+  
+  if(fondo>770){
+  nav.classList.add("active0")}
+  else{nav.classList.remove("active0")}
 }
 window.addEventListener("scroll", scrolled);
 
 
+// let jordan= document.getElementById("imagen6")
+// function JordanScrolled(){
+//   let fondo= window.pageYOffset
+  
+//   if(fondo>=2000){
+//   jordan.classList.add("fixed-bottom")}
+//   else{nav.classList.remove("fixed-bottom")}
+// }
+// window.addEventListener("scroll", JordanScrolled);
+// Scroll animations
+// Method1
+ScrollReveal().reveal("#navbar", {delay: 500})
+ScrollReveal().reveal(".carousel", {delay: 300})
+ScrollReveal().reveal(".subback", {delay: 400})
+ScrollReveal().reveal("#text-novelties", {delay: 500})
+ScrollReveal().reveal(".text0", {delay: 500})
+// ScrollReveal().reveal(".galery-two", {delay: 500})
 
+// Method2
+Window.sr= ScrollReveal();
+Window.sr.reveal(".imagen4", {
+  duration: 800,
+  origin: "left",
+  distance: "300px"
+})
+
+Window.sr.reveal("#imagen5", {
+  duration: 1300,
+  origin: "right",
+  distance: "300px"
+})
+Window.sr.reveal("#imagen6", {
+  duration: 1300,
+  origin: "bottom",
+  distance: "300px"
+})
+
+let svg1= document.getElementsByClassName("jordan")
+window.addEventListener(MouseEvent, ()=> {
+  
+
+})
+
+
+
+// SCROLL TRIES
 // Window.sr = ScrollReveal();
 // sr.reveal("#navbar", {
 //   duration: 2000,
